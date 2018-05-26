1e162a062066ac1c230a9bd29de62b36f9348b54
Baffo32 <baffo32@gmail.com>
2018-05-26 18:07:37 +0000

    add quick testing

--- tests/status.py
+++ tests/status.py
@@ -6,12 +6,19 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 10996
 BUFFER_SIZE = 1024
-MESSAGE = "q,sup,;\n"
+MSGSREPLIES = [
+        ("", "q,200,;\n"),
+        ("q,sup,;\n", "q,200,;\n")
+]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+for msgreply in MSGSREPLIES:
+    s.send(msgreply[0])
+    print "sent data:", repr(msgreply[0])
+    data = s.recv(BUFFER_SIZE)
+    print "received data:", repr(data)
+    if data != msgreply[1]:
+     raise AssertionError("Expected {} but got {}".format(repr(msgreply[1]), repr(data)))
 s.close()
 
-print "received data:", data
